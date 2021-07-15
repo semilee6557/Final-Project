c19e273909446c99111b477c3923833a22da6ff1
Semi Lee <semilee6557@gmail.com>
2021-07-15 08:01:18 +0000

    change come css and arrage display

--- client/pages/signUpSuccess.jsx
+++ client/pages/signUpSuccess.jsx
@@ -6,7 +6,7 @@ export default function SignUpSuccess(props) {
      <div className="mt-5 d-flex flex-column align-items-center">
         <div className='text-center p-5'>
           <h1>Welcome!</h1>
-          <h1>Please click sign in button to first login!</h1>
+          <h1>Please sign in to register and make appointment.</h1>
         </div>
         <a href='#signIn' type="button" className="btn btn-danger homeBtn">Sign In</a>
       </div>
